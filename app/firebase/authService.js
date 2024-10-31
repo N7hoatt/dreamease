75aadcfe6cc96ed9445fdd88560c6f9e866af8bc
N7hoatt <tranthaihoa1412@gmail.com>
2024-10-31 22:41:20 +0700

    auto update sleepcalculator

--- app/firebase/authService.js
+++ app/firebase/authService.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
+export const db = getFirestore(app);
 
 // Prompt user for email
 const getEmail = async () => {
